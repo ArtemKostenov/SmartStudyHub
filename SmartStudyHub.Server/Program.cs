38a3ef3efef80de08a5982c15b40851037436747
LionChikSan <kostenovartem@gmail.com>
2026-01-27 01:12:43 +0400

    Feat: Add CRUD for Notes

--- SmartStudyHub.Server/Program.cs
+++ SmartStudyHub.Server/Program.cs
@@ -3,18 +3,23 @@ using SmartStudyHub.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseOpenApi();
+    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
