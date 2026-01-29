89925865f75ae9a231a995a9b3d6f5d93d88474e
LionChikSan <kostenovartem@gmail.com>
2026-01-29 13:00:09 +0400

    Feat: Finish Auth Backend (JWT, AuthController, Protected Routes)

--- SmartStudyHub.Server/Program.cs
+++ SmartStudyHub.Server/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using SmartStudyHub.Server.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +12,54 @@ builder.Services.AddControllers();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!);
+
+builder.Services.AddAuthentication(options =>
+{
+   options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+   options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+   options.TokenValidationParameters = new TokenValidationParameters
+   {
+      ValidateIssuer = true,
+      ValidateAudience = true,
+      ValidateLifetime = true,
+      ValidateIssuerSigningKey = true,
+      ValidIssuer = jwtSettings["Issuer"],
+      ValidAudience = jwtSettings["Audience"],
+      IssuerSigningKey = new SymmetricSecurityKey(secretKey)
+   };
+});
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApiDocument();
+builder.Services.AddSwaggerGen(c =>
+{
+   c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+   {
+      Description = "Введите токен в формате: Bearer ey...",
+      Name = "Authorization",
+      In = ParameterLocation.Header,
+      Type = SecuritySchemeType.ApiKey,
+      Scheme = "Bearer"
+   });
+
+   c.AddSecurityRequirement(new OpenApiSecurityRequirement
+   {
+      {
+         new OpenApiSecurityScheme
+         {
+            Reference = new OpenApiReference
+            {
+               Type = ReferenceType.SecurityScheme,
+               Id = "Bearer"
+            }
+         },
+         new string[] {}
+      }
+   });
+});
 
 builder.Services.AddCors(options =>
 {
@@ -23,12 +73,13 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseOpenApi();
-    app.UseSwaggerUi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseCors("AllowReactApp");
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
