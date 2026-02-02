de05c0eba821afee5d683859cd7e06ead03a3ddd
LionChikSan <kostenovartem@gmail.com>
2026-02-02 19:51:37 +0400

    Feat: complete full CRUD notes on back

--- SmartStudyHub.Server/DTOs/CreateNoteRequest.cs
+++ SmartStudyHub.Server/DTOs/CreateNoteRequest.cs
@@ -4,7 +4,4 @@ public class CreateNoteRequest
 {
     public string Title {get; set;} = string.Empty;
     public string Content {get; set;} = string.Empty;
-
-    //Пока заглушка
-    public int UserId {get; set;}
 }
